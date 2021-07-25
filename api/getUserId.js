e45033dc3f5d0c4f967958748f29a8877f887a4c
OSUCapstone <87788437+OSUCapstone@users.noreply.github.com>
2021-07-24 20:12:59 -0400

    Create API endpoint to get user id

---
+++ api/getUserId.js
@@ -0,0 +1,46 @@
+const SQL = require("sql-template-strings");
+const mysql = require("serverless-mysql");
+
+// Initialize the database.
+const db = mysql({
+  config: {
+    host: "34.74.53.112",
+    user: "root",
+    password: "jobtracker",
+    database: "test",
+  },
+});
+
+// Main helper function that forms and ends a connection to the database with
+// each query.
+const query = async (query) => {
+  try {
+    const results = await db.query(query);
+    await db.end();
+    return results;
+  } catch (error) {
+    return { error };
+  }
+};
+
+module.exports = async (req, res) => {
+  try {
+    // Get user's id
+    let result = await query(
+      `SELECT user_id FROM token WHERE token = '${req.body.token}';`
+    );
+
+    // If token is valid
+    if (result[0]) {
+      return res.status(200).send(result[0].user_id);
+      
+    // If not, return unauthorized error
+    } else {          
+      return res.status(401).send();
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(503).send();
+  }
+};
+  
