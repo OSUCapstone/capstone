619721f86c6c8620e28809a3138d8a2b72543a84
garrisja <56104257+garrisja@users.noreply.github.com>
2021-08-07 11:16:40 -0700

    Job page now requests info about job

--- src/pages/JobPage.js
+++ src/pages/JobPage.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { withRouter } from "react-router-dom";
 
-import { Heading } from "../components";
+import { Heading, Button } from "../components";
+import { requestPost } from "../requests";
 
 const JobPage = withRouter(({ match, history, location }) => {
+  const [job, setJob] = useState("");
+  
+  useEffect(() => {
+    const init = async () => {      
+      // Extract job_id from route
+      let id = location.pathname;
+      id = id.substr(id.lastIndexOf('/') + 1);
+      
+      // Read job information
+      let res = await requestPost("/api/crudJob", { crud: "read", job_id: id });
+      if (res) {
+        console.log(res);
+        setJob(res);
+      } else {
+        setJob([]);
+      }
+    };
+    init ();
+  }, []);
+
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
-      <Heading>Job Page</Heading>
+      {job == null && <Heading>Loading...</Heading>}
+      {job && <Heading>{job.job_title}</Heading>}
     </div>
   );
 });
